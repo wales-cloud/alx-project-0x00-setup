92f6890398e99b3b78f04eb46709afdabdafee89
Adewale Salami <wales.abbe@gmail.com>
2025-06-01 15:30:59 -0600

    Use Card component in landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,16 @@
+import React from "react";
+import Card from "@/components/Card";
+
 const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className="text-xl font-extralight">Landing Page</h1>
+    <div className="p-6">
+      <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
+      
+      <div className="flex flex-wrap gap-6">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
